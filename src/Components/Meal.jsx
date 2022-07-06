d9599bc066127bb81b1b1c6b06745ca11ad1dbe1
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-07-06 19:16:46 +0200

    styled the meal component

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Meal() {
+  return (
+    <div className='main'>
+      <div className='heading'>
+        <h1>Search Your Favourite Food</h1>
+        <h4>You can search and order the best food you can even imagine</h4>
+      </div>
+      <div className='searchBox'>
+        <input type='search' />
+      </div>
+    </div>
+  );
+}
