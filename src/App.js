4728e4803bba2f11bac8826c090e056b40da2195
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-07-07 09:24:19 +0200

    added the functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React from 'react';
 import Meal from './Components/Meal';
 import './Components/style.css';
-
+import { Routes, Route } from 'react-router-dom';
+import RecipeInfo from './Components/RecipeInfo';
 function App() {
   return (
     <>
-      <Meal />
+      <Routes>
+        <Route path='/' element={<Meal />} />
+        <Route path='/:MealId' element={<RecipeInfo />} />
+      </Routes>
     </>
   );
 }
--- src/Components/Meal.jsx
+++ src/Components/Meal.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
-
+import { useEffect } from 'react';
+import { useState } from 'react';
+import MealItem from './MealItem';
+import RecipeIndex from './RecipeIndex';
 export default function Meal() {
+  const [url, setUrl] = useState(
+    'https:/www.themealdb.com/api/json/v1/1/search.php?f=a'
+  );
+  const [item, setItem] = useState();
+  const [show, setShow] = useState(false);
+  const [search, setSearch] = useState('');
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.meals);
+        setItem(data.meals);
+        setShow(true);
+      });
+  }, [url]);
+  const setIndex = (alpha) => {
+    setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
+  };
+
+  const searchRecipe = (evt) => {
+    if (evt.key == 'Enter') {
+      setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+    }
+  };
   return (
     <div className='main'>
       <div className='heading'>
@@ -8,7 +35,18 @@ export default function Meal() {
         <h4>You can search and order the best food you can even imagine</h4>
       </div>
       <div className='searchBox'>
-        <input type='search' />
+        <input
+          type='search'
+          className='search-bar'
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyPress={searchRecipe}
+        />
+      </div>
+      <div className='container'>
+        {show ? <MealItem data={item} /> : 'The food is not found'}
+      </div>
+      <div className='indexContainer'>
+        <RecipeIndex alphaIndex={(alpha) => setIndex(alpha)} />
       </div>
     </div>
   );
